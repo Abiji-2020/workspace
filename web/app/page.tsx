ae9241a1efc8e88705415c5f036359066a845ac7
Rageshwaran-HR <rageshwaranhr@gmail.com>
2024-12-22 23:15:39 +0530

    signin page front end

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,10 @@
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    
+    <AuthScreen/>
    </div>
   );
 }
